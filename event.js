5748381e7310b66f07a1917fd7394547fcaf7bad
Gangadhara-Rao <86068845+Gangadhara-Rao@users.noreply.github.com>
2022-06-23 20:23:35 +0530

    commit

--- event.js
+++ event.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+var events = require('events');
+
+var myEmmitter = new events.EventEmitter();
+
+myEmmitter.on('anEvent',function(msg){
+    console.log(msg);
+});
+
+myEmmitter.emit('anEvent','The event is absolutely emmited');
