3f10b51a0477bf9dc08f400086942eaabbf29f0e
Gangadhara-Rao <86068845+Gangadhara-Rao@users.noreply.github.com>
2022-06-28 15:21:46 +0530

    commit

---
+++ closure.js
@@ -0,0 +1,11 @@
+
+function createAdder(baseNumber){
+	return function(numberToAdd){
+		return baseNumber + numberToAdd;
+	}
+}
+
+var test1 = createAdder(10);
+
+console.log(test1(2));
+console.log(test1(0));
