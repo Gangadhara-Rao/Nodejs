f3e3d78663c63505fff359cd0ee72020cf237415
Gangadhara-Rao <86068845+Gangadhara-Rao@users.noreply.github.com>
2022-06-28 14:31:36 +0530

    commit

---
+++ array.js
@@ -0,0 +1,11 @@
+var totalGrade  = 0;
+
+grades.push(98);
+
+grades.forEach(function (grades){
+
+	totalGrade += grades;
+	console.log('Current grade is '+ totalGrade);
+});
+
+console.log('Average grade = '+ totalGrade/grades.length);
