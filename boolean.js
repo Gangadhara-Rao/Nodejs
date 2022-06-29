523e6e04248f05a2faec49d384df3158e0dbc74e
Gangadhara-Rao <86068845+Gangadhara-Rao@users.noreply.github.com>
2022-06-29 12:04:41 +0530

    commit

---
+++ boolean.js
@@ -0,0 +1,13 @@
+function Boolean(variable){
+    if(typeof variable == 'boolean'){
+
+        return !variable;
+
+    }else{
+
+        console.log('Warning! not a boolean');
+
+    }
+
+}
+console.log(Boolean('df'));
